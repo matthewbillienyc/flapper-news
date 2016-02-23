9e19dbe00d74fb1acef0c90a211b6b7c424a0c24
matthewbillienyc <matthewbillienyc@gmail.com>
2016-02-23 01:12:05 -0500

    posts, comments, users

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose');
+var passport = require('passport');
+require('./models/Posts');
+require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -14,6 +19,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// connect to mongodb
+mongoose.connect('mongodb://localhost/news');
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -21,9 +29,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(passport.initialize());
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+// var express = require('express');
+// var router = express.Router();
+// var mongoose = require('mongoose');
+// var passport = require('passport');
+// var User = mongoose.model('User');
+//
+// /* GET users listing. */
+// router.post('/register', function(req, res, next){
+//   if(!req.body.username || !req.body.password){
+//     return res.status(400).json({ message: 'Fill in all fields.' });
+//   }
+//   var user = new User();
+//   user.username = req.body.username;
+//   user.setPassword(req.body.password);
+//   user.save(function(err){
+//     if(err){ return next(err); }
+//     return res.json({ token: user.generateJWT() });
+//   });
+// });
+//
+// router.post('/login', function(req, res, next){
+//   if(!req.body.username || !req.body.password){
+//     return res.status(400).json({ message: 'Fill in all fields.' });
+//   }
+//   passport.authenticate('local', function(err, user, info){
+//     if(err){ return next(err); }
+//     if(user){
+//       return res.json({ token: user.generateJWT() });
+//     } else {
+//       return res.status(401).json(info);
+//     }
+//   })(req, res, next);
+// });
+//
+// module.exports = router;
